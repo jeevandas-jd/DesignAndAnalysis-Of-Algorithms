f58e22101eca2fdce08b0ff4621804309059123e
jeevandas-jd <jeevandasms2004@gmail.com>
2024-08-27 13:34:30 +0530

    dijkstras

--- src/graph/dijikstrs.cpp
+++ src/graph/dijikstrs.cpp
@@ -1,22 +1,62 @@
 #include <iostream>
 
+#include <climits>
+
 using namespace std;
-#de
-class graph{
-    public:
-    int cost[100][100];
-    int Nodes;
+int MinDistace(int dist[3],bool flag[3]){
 
-    graph(int n){
-        this->Nodes=n;
-    }
-
-    void setCost(){
-        for(int i =0;i<Nodes;i++){
-            for(int j=0;j<Nodes;j++){
-                cout<<"enter distance between "<<i<<"And"<<j<<endl;
-                cin>>cost[i][j];
-            }
+    int min=INT16_MAX;
+    int index;
+    for(int i =0;i<3;i++){
+        if(flag[i]==false && dist[i]<min){
+            index=i;
+            min=dist[i];
         }
     }
+    cout<<"first index = "<<index<<endl;
+    return index;
+
+}
+
+void relax();
+void dijkstra(int graph[3][3],int src){
+
+    int dist[3];
+    bool flags[3];
+
+    for(int i =0;i<3;i++){
+        dist[i]=INT16_MAX;
+        flags[i]=false;
+    }
+
+    dist[src]=0;
+
+    for(int i=0;i<3;i++){
+        int m=MinDistace(dist,flags);
+        flags[m]=true;
+        for(int j=0;j<3;j++){
+            if(flags[j]==false && graph[m][j]!=0 && dist[m]<INTMAX_MAX && dist[m]+graph[m][j]<dist[j]){
+                dist[j]=dist[m]+graph[m][j];
+            }
+
+        }
+        
+    }
+    char str;
+    for(int i=0;i<3;i++){
+        str=65+i;
+        cout<<str<<"\t\t\t"<<dist[i]<<endl;
+    }
+}
+
+int main(){
+   int graph[3][3] = {
+    {0, 3, 1},
+    {3, 0, 7},
+    {1, 7, 0}
 };
+
+
+    dijkstra(graph,0);
+}
+
