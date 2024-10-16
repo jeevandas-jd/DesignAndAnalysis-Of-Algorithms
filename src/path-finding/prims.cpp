35b6f86b34ce7a73b2480d78252779c1f580eade
jeevandas-jd <jeevandasms2004@gmail.com>
2024-10-16 15:22:59 +0530

    primsMST

---
+++ src/path-finding/prims.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+int minKey(int key[],bool mstSet[]){
+
+}
+
+
+void PRIMSmst(int graph[][],int nodes){
+
+    int parent[nodes];
+    int MST[nodes];
+
+    int key[nodes];
+
+    bool MSTset[nodes];
+
+    for(int i=0;i<nodes;i++){
+        key[i]=INT16_MAX;
+        MST[i]=false;
+    }
+
+    key[0]=[0];
+    parent[0]=-1;
+    
+
+}
